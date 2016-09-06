7ece176510de556a965a268206512d6610504049
David Matterne <david.matterne@gmail.com>
2016-09-06 15:54:27 +0200

    Module config and run

--- finished_template_gulp_2016/src/app/app.module.js
+++ finished_template_gulp_2016/src/app/app.module.js
@@ -2,3 +2,11 @@
  * Created by stannisbaratheon on 06/09/16.
  */
 var contactsApp = angular.module('contactsApp', ['contactsApp.core']);
+
+contactsApp.config(function() {
+    console.log('app - config');
+});
+
+contactsApp.run(function() {
+    console.log('app - run');
+});
--- finished_template_gulp_2016/src/app/core/core.module.js
+++ finished_template_gulp_2016/src/app/core/core.module.js
@@ -1,4 +1,10 @@
 /**
  * Created by stannisbaratheon on 06/09/16.
  */
-angular.module('contactsApp.core',[]);
+angular.module('contactsApp.core',[])
+    .config(function() {
+        console.log('core - config');
+    })
+    .run(function() {
+        console.log('core - run');
+    });
